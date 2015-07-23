32946fe75dd2b97a27aa743361d28947b298f0b0
Josh McCulloch <mcculloch@windowslive.com>
2015-07-24 10:40:55 +1200

    Added Transform class. This allows a ridgid body to be moved around the board rather than needing to be instantiated in the instantaneous location

--- Phys.js
+++ Phys.js
@@ -25,6 +25,10 @@
         return new Vec2d(this.x - v.x, this.y- v.y);
     };
 
+    Vec2d.prototype.mul = function (p) {
+        return new Vec2d(this.x * p.x, this.y * p.y);
+    };
+
     Vec2d.prototype.scale = function (s) {
         return new Vec2d(this.x * s, this.y * s);
     };
@@ -40,6 +44,30 @@
     Vec2d.prototype.reflect = function (normal) {
         var n = normal.normalised();
         return this.sub(n.scale(2* this.dot(n)));
+    };
+
+    function Transform (trans, scale, rot) {
+        this.trans = trans;
+        this.scale = scale;
+        this.rot = rot;
+        this.cos = Math.cos(rot);
+        this.sin = Math.sin(rot);
+        this.nsin = -Math.sin(rot);
+    }
+    exports.Transform = Transform;
+
+    Transform.prototype.transformPoint = function (p) {
+        //Apply scale
+        p = p.mul(this.scale);
+
+        //Apply Rotation
+        p = new Vec2d(
+            p.x * this.cos + p.y * this.nsin,
+            p.x * this.sin + p.y * this.cos
+        );
+
+        //Apply translation
+        return p.add(this.trans);
     };
 
     function Line(p1, p2) {
@@ -62,22 +90,52 @@
         } else {
             return this.a.add(a2b.normalised().scale(t));
         }
-    }
+    };
 
     function ConvexHull (points) {
+        this.originalPoints = points
+        this.points = points;
         this.lines = [];
-        for(var p=0; p<(points.length-1); p++) {
+        this.transform = new Transform(0,1,0);
+        this.updatePoints();
+        this.updateLines();
+    }
+    exports.ConvexHull = ConvexHull;
+
+    ConvexHull.prototype.setTransform = function (transform) {
+        this.transform = transform;
+        this.updatePoints();
+        this.updateLines();
+    };
+
+    ConvexHull.prototype.updatePoints = function () {
+        this.points = [];
+        for(var p=0; p<(this.originalPoints.length); p++) {
+            this.points.push(this.transform.transformPoint(this.originalPoints[p]));
+        }
+    };
+
+    ConvexHull.prototype.updateLines = function () {
+        for(var p=0; p<(this.points.length-1); p++) {
             this.lines.push(new Line(
-                    points[p].clone(),
-                    points[p+1].clone()
+                this.points[p].clone(),
+                this.points[p+1].clone()
             ));
         }
         this.lines.push(new Line(
-                points[points.length-1].clone(),
-                points[0].clone()
+            this.points[this.points.length-1].clone(),
+            this.points[0].clone()
         ));
-    }
-    exports.ConvexHull = ConvexHull;
+    };
+
+    ConvexHull.prototype.render = function (context) {
+        context.beginPath();
+        context.moveTo(this.points[this.points.length-1].x, this.points[this.points.length-1].y);
+        for(var p=0; p<(this.points.length); p++) {
+            context.lineTo(this.points[p].x, this.points[p].y);
+        }
+        context.stroke();
+    };
 
     function Circle(p, r) {
         this.p = p;
@@ -97,8 +155,9 @@
             }
         }
         return {
-            intersects: distance <= this.r,
+            intersects: distance < this.r,
             distance: distance,
+            overlap: closestPoint.sub(this.p).scale(1-distance/this.r),
             point: closestPoint
         };
 
